219d0904f723cb4c16037ad4830e713fe466fc31
snikithareddykovvuri-jpg <snikithareddykovvuri@gmail.com>
2025-11-21 12:51:02 +0530

    Create else if multiples of 6 or 9.cpp

---
+++ else if multiples of 6 or 9.cpp
@@ -0,0 +1,13 @@
+//write a c program to display YES if it is multiple of 6 or 9 otherwise display NO
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter a number:");
+	scanf("%d",&n);
+	if(n%6==0,n%9==0)
+	printf("YES\n");
+	else 
+	printf("NO\n");
+	return 0;
+}
