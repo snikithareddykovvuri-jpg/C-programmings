02f785218f917d320c143653b9b147ad44c910db
snikithareddykovvuri-jpg <snikithareddykovvuri@gmail.com>
2025-11-21 12:29:32 +0530

    Create else if alphabet or not.cpp

---
+++ else if alphabet or not.cpp
@@ -0,0 +1,13 @@
+//Write a c program to check whether the given character is alphabet or not
+#include<stdio.h>
+int main()
+{
+	char ch;
+	printf("enter a character:");
+	scanf("%c",&ch);
+	if((ch>=65&&ch<=90)||(ch>=97&&ch<=122))
+	printf("YES\n");
+	else
+	printf("NO\n");
+	return 0;
+}
