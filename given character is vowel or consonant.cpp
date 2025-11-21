2092862350ab85b84e0ce8cdbb7cd5421fd525df
snikithareddykovvuri-jpg <snikithareddykovvuri@gmail.com>
2025-11-21 11:51:56 +0530

    Create given character is vowel or consonant.cpp

---
+++ given character is vowel or consonant.cpp
@@ -0,0 +1,29 @@
+//write a c program to find the given character is vowel or consonant                              
+#include<stdio.h>
+int main()
+{
+	char ch;
+	printf("enter a character:");
+	scanf("%c",&ch);
+	if((ch>='A'&&ch<='Z')||(ch>='a'&&ch<='z'))
+	{
+		switch(ch)
+		{
+			case'a':
+			case'A':
+			case'e':
+			case'E':
+			case'i':
+			case'I':
+			case'o':
+			case'O':
+			case'u':
+			case'U':printf("given character is vowel\n");
+			        break;
+			default:printf("given character is consonant\n");
+		}
+	}
+	else
+	printf("given character is not an alphabet\n");
+	return 0;
+  }  
