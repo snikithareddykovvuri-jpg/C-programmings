24b1dda4758b6a7bdb7add31aea0e930bb8b6b03
snikithareddykovvuri-jpg <snikithareddykovvuri@gmail.com>
2025-11-21 11:53:12 +0530

    Create if else if to check lowercase uppercase digits.cpp

---
+++ if else if to check lowercase uppercase digits.cpp
@@ -0,0 +1,17 @@
+//write a c program to check the given character is lowercase or uppercase or digits
+#include<stdio.h>
+int main()
+{
+	char ch;
+	printf("enter a character:");
+	scanf("%c",&ch);
+	if(ch>='A'&&ch<='Z')
+	printf("uppercase\n");
+	else if(ch>='a'&&ch<='z')
+	printf("lowercase\n");
+	else if(ch>='0'&&ch<='9')
+	printf("digit\n");
+	else
+	printf("neither a digit nor alphabet\n");
+	return 0;
+}
