4d378744b9984cd23248b62f8bf1d14ea560b60c
snikithareddykovvuri-jpg <snikithareddykovvuri@gmail.com>
2025-11-21 11:59:29 +0530

    Create palindrome using while loop.cpp

---
+++ palindrome using while loop.cpp
@@ -0,0 +1,22 @@
+//write a c program to check the given number is palindrome or not using while loop
+#include<stdio.h>
+int main()
+{
+	int rev=0,n,r,m;
+	printf("enter a number:");
+	scanf("%d",&n);
+	m=n;
+	while(n!=0)
+	{
+		r=n%10;
+		rev=rev*10+r;
+		n=n/10;
+	}
+	if(rev==m)
+	{
+		printf("palindrome\n");
+	}
+	else
+	printf("not a palindrome\n");
+	return 0;
+}
