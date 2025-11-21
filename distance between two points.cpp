1f4fffef58d31df2c567cd8dbfff8c9a6d9ab95b
snikithareddykovvuri-jpg <snikithareddykovvuri@gmail.com>
2025-11-21 12:28:10 +0530

    Create distance between two points.cpp

---
+++ distance between two points.cpp
@@ -0,0 +1,15 @@
+//write a c program to find the distance between two points on a 2D plane
+ #include<stdio.h>
+ #include<math.h>
+ int main()
+ {
+ 	int x1,y1,x2,y2;
+ 	float d;
+ 	printf("Enter first point(x1,y1):");
+ 	scanf("%d%d",&x1,&y1);
+ 	printf("Enter second point(x2,y2):");
+ 	scanf("%d%d",&x2,&y2);
+ 	d=sqrt(pow(x2-x1,2)+pow(y2-y1,2));
+ 	printf("Distance = %.2f\n",d);
+ 	return 0;
+ }
